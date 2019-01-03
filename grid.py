dd06243604255ebf4b39d69bee67eb7255ef8975
elvittt <46191365+elvittt@users.noreply.github.com>
2019-01-03 04:54:16 -0500

    Add files via upload

---
+++ grid.py
@@ -0,0 +1,32 @@
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+#def image(alist):
+    #for x in range(len(alist)):
+        #print(alist[x][0],end='')
+    #print('')
+    #for x in range(len(alist)):
+        #print(alist[x][1],end='')  
+    #print('')
+    #for x in range(len(alist)):
+        #print(alist[x][2],end='')   
+    #print('')
+    #for x in range(len(alist)):
+         #print(alist[x][3],end='')  
+    #print('')
+    #for x in range(len(alist)):
+         #print(alist[x][4],end='')   
+    #print('')
+    #for x in range(len(alist)):
+         #print(alist[x][5],end='')
+
+
+
+image(grid)
